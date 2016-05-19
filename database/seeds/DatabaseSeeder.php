c69d857a2aa0c9d500058b502eda6247a50b2bfa
BuildTools <me@renophilibert.com>
2016-05-18 23:15:37 -0400

    It's like we actually have more products or something now

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,26 @@ class DatabaseSeeder extends Seeder
         $this->call(ProductSeederAC::class);
         $this->call(ProductSeederAD::class);
         $this->call(ProductSeederAE::class);
+        $this->call(ProductSeederAF::class);
+        $this->call(ProductSeederAG::class);
+        $this->call(ProductSeederAH::class);
+        $this->call(ProductSeederAI::class);
+        $this->call(ProductSeederAJ::class);
+        $this->call(ProductSeederAK::class);
+        $this->call(ProductSeederAL::class);
+        $this->call(ProductSeederAM::class);
+        $this->call(ProductSeederAN::class);
+        $this->call(ProductSeederAO::class);
+        $this->call(ProductSeederAP::class);
+        $this->call(ProductSeederAQ::class);
+        $this->call(ProductSeederAR::class);
+        $this->call(ProductSeederAS::class);
+        $this->call(ProductSeederAT::class);
+        $this->call(ProductSeederAU::class);
+        $this->call(ProductSeederAV::class);
+        $this->call(ProductSeederAW::class);
+        $this->call(ProductSeederAX::class);
+        $this->call(ProductSeederAY::class);
+        $this->call(ProductSeederAZ::class);
     }
 }
